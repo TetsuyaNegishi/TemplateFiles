98b26034823e39a18180bcf3dfd6f74a36312fbf
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-01-03 22:19:38 +0900

    [update]。、を．，に変換する処理を追加

--- removeTightlist.js
+++ removeTightlist.js
@@ -4,6 +4,8 @@ const inputFilePath = process.argv[2];
 const buf = fs.readFileSync(inputFilePath);
 let str = buf.toString();
 str = str.replace(/\\tightlist/g, '');
+str = str.replace(/、/g, '，');
+str = str.replace(/。/g, '．');
 
 const data = str;
 fs.writeFile(inputFilePath, data, (err) => {
